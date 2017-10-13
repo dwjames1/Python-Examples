b136beffedb65311f14aa4fe9c11edeaee6d0b7b
djames <djames@freewave.com>
2017-10-13 15:48:03 -0600

    End of day

--- src/manipulate_sheets.py
+++ src/manipulate_sheets.py
@@ -4,17 +4,14 @@ Created on Oct 6, 2017
 @author: djames
 '''
 
-import openpyxl
 from openpyxl import load_workbook
-from openpyxl import chart
-from openpyxl.styles import Alignment, NamedStyle, Font, Border, Side, PatternFill
-from openpyxl.styles.differential import DifferentialStyle
-from openpyxl.utils import get_column_letter
-from openpyxl.formatting.rule import ColorScaleRule, CellIsRule, FormulaRule
-from openpyxl.formatting import Rule
 
-from openpyxl.styles.colors import RED
-from openpyxl.styles.fills import FILL_NONE
+from openpyxl.styles import Alignment, NamedStyle, Font, Border, Side, PatternFill
+
+from openpyxl.utils import get_column_letter
+from openpyxl.formatting.rule import CellIsRule
+from openpyxl.styles.differential import DifferentialStyle
+
 
 # Auto Width
 # https://stackoverflow.com/questions/13197574/openpyxl-adjust-column-width-size
@@ -24,43 +21,55 @@ from openpyxl.styles.fills import FILL_NONE
 # ws.column_dimensions.group(start='B', end='CU', hidden=True)
 
 
-'''
->>> from openpyxl.styles import NamedStyle, Font, Border, Side
->>> highlight = NamedStyle(name="highlight")
->>> highlight.font = Font(bold=True, size=20)
->>> bd = Side(style='thick', color="000000")
->>> highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+###
+# from openpyxl.styles import NamedStyle, Font, Border, Side
+# highlight = NamedStyle(name="highlight")
+# highlight.font = Font(bold=True, size=20)
+# bd = Side(style='thick', color="000000")
+# highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
 
-'''
-thin_border = Border(left=Side(style='thin'), 
-                     right=Side(style='thin'), 
-                     top=Side(style='thin'), 
+
+THIN_BORDER = Border(left=Side(style='thin'),
+                     right=Side(style='thin'),
+                     top=Side(style='thin'),
                      bottom=Side(style='thin'))
-redFill = PatternFill(start_color='FF9999',
-                      end_color='FF9999',
-                      fill_type='solid')
-greenFill = PatternFill(start_color='CCFF99',
-                      end_color='CCFF99',
-                      fill_type='solid')
+RED_FILL = PatternFill(start_color='FF9999',
+                       end_color='FF9999',
+                       fill_type='solid')
+GREEN_FILL = PatternFill(start_color='CCFF99',
+                         end_color='CCFF99',
+                         fill_type='solid')
 
- 
-VERT_STYLE = NamedStyle(name='VERT_STYLE') # Style for parameters that shoudl be rotated
+
+VERT_STYLE = NamedStyle(name='VERT_STYLE')  # Style for parameters that shoudl be rotated
 VERT_STYLE.alignment = Alignment(textRotation=90, vertical='bottom', horizontal='center')
 VERT_STYLE.font = Font(bold=True)
-VERT_STYLE.border = thin_border
+VERT_STYLE.border = THIN_BORDER
+
+VERT2 = DifferentialStyle()
+VERT2.alignment = Alignment(textRotation=90, vertical='bottom', horizontal='center')
 
 # List of sheets in run report and compare that should rotate line 2.
-VERT_ROWS_SHEETS = ['Result', 'Results', 'CommonParamFlat', 'Results Exceeding Tolerance', 'aleph', 'bet']
+VERT_ROWS_SHEETS = ['Result', 'Results', 'CommonParamFlat',
+                    'Results Exceeding Tolerance', 'aleph', 'bet']
+
 
 def set_row_style(sheet, row, style):
+    """
+    apply the given style to an entire row
+    :param sheet: openpyxl sheet
+    :param row: int, row number
+    :param style: openpyxl styel
+    """
 
     for cell in sheet[row]:
         cell.style = style
 
+
 def col_match(sheet, row, value):
     """
     find all columns matching value
-    
+
     Return first matching column
     :param sheet: an openpyxl sheet
     :param row:   row descriptor (e.g. '2:2')
@@ -70,51 +79,61 @@ def col_match(sheet, row, value):
         if cell.value == value:
             return cell.column
 
+
 def auto_width(sheet):
+    """
+    Adjust the column width, mimicking Excel's auto function
+
+    Must approximate for variable width columns
+    :param sheet: an openpyxl sheet object.
+    """
     column_widths = []
     for row in sheet.iter_rows():
         for i, cell in enumerate(row):
             try:
-                #if cell alignment is vertical, use 4, else len(str(cell.value)
+                # if cell alignment is vertical, use 4, else len(str(cell.value)
                 if cell.alignment.textRotation == 90:
                     cell_w = 4
                 else:
                     cell_w = len(str(cell.value))
-                column_widths[i] = max(column_widths[i],cell_w)
+                column_widths[i] = max(column_widths[i], cell_w)
             except IndexError:
                 column_widths.append(len(str(cell.value)))
 
     for i, column_width in enumerate(column_widths):
         # 1.4 is a fudge factor for variable width fonts
-        sheet.column_dimensions[get_column_letter(i + 1)].width = column_width*1.4
-        
+        sheet.column_dimensions[get_column_letter(i + 1)].width = column_width * 1.4
+
+
 def main():
     '''Main'''
-    
-    wb = load_workbook(filename='51-SysTstDev-192-SysTstDev-5.xlsx')
-    print(col_match(wb['Results'], '2:2', 'diff'))
+
+    my_wb = load_workbook(filename='51-SysTstDev-192-SysTstDev-5.xlsx')
+    print(col_match(my_wb['Results'], '2:2', 'diff'))
+    tclm = col_match(my_wb['Results'], '2:2', 'diff')
+    thresh_column = tclm + ':' + tclm
     for sheet in VERT_ROWS_SHEETS:
         try:
-            set_row_style(wb[sheet], '2:2', VERT_STYLE )
-        except:
+            set_row_style(my_wb[sheet], '2:2', VERT_STYLE)
+        except KeyError:
             pass
-    for sheet in  wb.get_sheet_names():
-        auto_width(wb[sheet])
-    
-    wb['Results'].column_dimensions.group(start='K', end='Q', hidden=True)
+    for sheet in my_wb.get_sheet_names():
+        auto_width(my_wb[sheet])
 
-    wb['Results'].conditional_formatting.add('AZ:AZ',
-                                             CellIsRule(operator='greaterThan',
-                                                        formula=['5'],
-                                                        fill=greenFill,
-                                                        stopIfTrue=True))
-    wb['Results'].conditional_formatting.add('AZ:AZ',
-                                             CellIsRule(operator='lessThan',
-                                                        formula=['-5'],
-                                                        fill=redFill,
-                                                        stopIfTrue=True))
-    wb.active = wb.index(wb['Results'])  #TODO Result for report
-    wb.save(filename='51-SysTstDev-192-SysTstDev-5PLUS.xlsx')
-    
+    my_wb['Results'].column_dimensions.group(start='K', end='Q', hidden=True)
+
+    my_wb['Results'].conditional_formatting.add(thresh_column,
+                                                CellIsRule(operator='greaterThan',
+                                                           formula=['5'],
+                                                           fill=GREEN_FILL,
+                                                           stopIfTrue=True))
+    my_wb['Results'].conditional_formatting.add('AZ:AZ',
+                                                CellIsRule(operator='lessThan',
+                                                           formula=['-5'],
+                                                           fill=RED_FILL,
+                                                           stopIfTrue=True))
+    my_wb.active = my_wb.index(my_wb['Results'])  # TODO Result for report
+    my_wb.save(filename='51-SysTstDev-192-SysTstDev-5PLUS.xlsx')
+
 if __name__ == '__main__':
     main()
--- src/pandas_merge.py
+++ src/pandas_merge.py
@@ -19,51 +19,51 @@ from manipulate_sheets import auto_width
 # import matplotlib.pyplot as plt
 
 
-
 DF1 = pd.DataFrame({"P1": "enabled",
                     "P2":  ["on", "on", "off", "off"],
                     "P3": ["on", "off", "on", "off"],
-                    "R"  : [1, 2, 3, 4]
-                  })
+                    "R": [1, 2, 3, 4]
+                    })
 
 DF1C = pd.DataFrame({"P1": "enabled",
-                    "P2":  ["on", "on", "off", "off"],
-                    "P3": ["on", None, "on", None],
-                    "R"  : [1, 2, 3, 4]
-                  })
-#short DF1
+                     "P2":  ["on", "on", "off", "off"],
+                     "P3": ["on", None, "on", None],
+                     "R": [1, 2, 3, 4]
+                    })
+# short DF1
 DF1B = pd.DataFrame({"P1": "enabled",
-                    "P2":  ["on", "on", "off"],
-                    "P3": ["on", "off", "on"],
-                    "R"  : [1, 2, 3]
-                  })
+                     "P2":  ["on", "on", "off"],
+                     "P3": ["on", "off", "on"],
+                     "R": [1, 2, 3]
+                    })
 
 DF1D = pd.DataFrame({"P1": "ReallyReallyReallyReally wide",
-                    "P2":  ["on", "on", "off", "off"],
-                    "P3": ["on", "off", "on", "off"],
-                    "R"  : [1, 2, 3, 4]
-                  })
+                     "P2":  ["on", "on", "off", "off"],
+                     "P3": ["on", "off", "on", "off"],
+                     "R": [1, 2, 3, 4]
+                    })
 
 DF2 = pd.DataFrame({"P1": "enabled",
                     "P2":  ["on", "on", "off", "off"],
                     "P3": ["on", "off", "on", "off"],
-                    "R"  : [5, 6, 7, 8]
-                  })
-#extra 'P" column
+                    "R": [5, 6, 7, 8]
+                   })
+# extra 'P" column
 DF3 = pd.DataFrame({"P1": "enabled",
                     "P2":  ["on", "on", "off", "off"],
                     "P3": ["on", "off", "on", "off"],
                     "P4": ["on", "off", "on", "off"],
-                    "R"  : [11, 12, 13, 14]
-                  })
+                    "R": [11, 12, 13, 14]
+                   })
 
-#shorter
+# shorter
 DF4 = pd.DataFrame({"P1": "enabled",
                     "P2":  ["on", "on", "off"],
                     "P3": ["on", "off", "on"],
                     "P4": ["on", "off", "on"],
-                    "R"  : [15, 16, 17]
-                  })
+                    "R": [15, 16, 17]
+                   })
+
 
 def name_of_object(arg):
     """
@@ -83,15 +83,15 @@ def name_of_object(arg):
             return name
 
 
-
-def dpp(dataframe, cap = ''):
+def dpp(dataframe, cap=''):
     """
     Pretty print a dataframe, with name and optional caption
     :param dataframe:
     :param cap: caption  - string to include in the name line
     """
-    print("\r" + cap + "(" + (name_of_object(dataframe) or '') +  "):")
+    print("\r" + cap + "(" + (name_of_object(dataframe) or '') + "):")
     pp(dataframe)
+
 
 def merges():
     """
@@ -107,18 +107,16 @@ def merges():
 
     # as successful merge,
 
-    '''left_on and right_on must be the same length, default suffixes ar
-    _x and _y
-    '''
+    # left_on and right_on must be the same length, default suffixes are     _x and _y
+
     results2 = DF1.merge(DF2, left_on=["P1", "P2", "P3"], right_on=["P1", "P2", "P3"])
     dpp(results2)
 
-    #Merge on two
+    # Merge on two
     results3 = DF1.merge(DF2, left_on=["P2", "P3"], right_on=["P2", "P3"])
     dpp(results3, cap="Merge on P2 and P3")
 
     # hm... looks like the order is x, commmon x, y
-
 
     # merge, with extra column in b
     dpp(DF1)
@@ -130,11 +128,12 @@ def merges():
     try:
         dpp(DF1)
         dpp(DF3)
-        results5 = DF1.merge(DF3, left_on=["P1", "P2", "P3", "P4"], right_on=["P1", "P2", "P3", "P4"])
+        results5 = DF1.merge(DF3, left_on=["P1", "P2", "P3", "P4"],
+                             right_on=["P1", "P2", "P3", "P4"])
         dpp(results5)
     except KeyError as err:
         print(repr(err))
-        print ("join keys must be the same length, and present in their DF")
+        print("join keys must be the same length, and present in their DF")
 
     # merge shorter into longer
     dpp(DF4)
@@ -147,12 +146,12 @@ def merges():
     result6 = DF1B.merge(DF2, left_on=["P1", "P2", "P3"], right_on=["P1", "P2", "P3"])
     dpp(result6, cap="Fewer rows on left than right")
 
-
     # merger on a subset
     dpp(DF3)
     dpp(DF4)
     result7 = DF3.merge(DF4, left_on=["P1", "P3"], right_on=["P1", "P3"])
     dpp(result7, cap="subset of parameters")
+
 
 def find_and_swap():
     '''A set of table manipulations'''
@@ -160,14 +159,15 @@ def find_and_swap():
     # https://stackoverflow.com/questions/18196203/how-to-conditionally-update-dataframe-column-in-pandas
 
     dpp(DF1)
-    DF1.loc[DF1['P3'] == 'off', 'P1'] =' disabled'
+    DF1.loc[DF1['P3'] == 'off', 'P1'] = ' disabled'
     dpp(DF1)
 
     dpp(DF1C)
-    DF1C.loc[DF1C['P3'].isnull(), 'P1'] =' disabled'
+    DF1C.loc[DF1C['P3'].isnull(), 'P1'] = ' disabled'
     dpp(DF1C)
-    DF1C.loc[DF1C['P3'].isnull(), 'P3'] ='off'
+    DF1C.loc[DF1C['P3'].isnull(), 'P3'] = 'off'
     dpp(DF1C, cap="with P3 'fixed'")
+
 
 def excel_format():
     '''format in memory'''
@@ -183,6 +183,6 @@ def excel_format():
 
 if __name__ == '__main__':
 
-#     merges()
-#     find_and_swap()
+    #     merges()
+    #     find_and_swap()
     excel_format()
